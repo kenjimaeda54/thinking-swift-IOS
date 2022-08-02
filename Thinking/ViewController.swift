2855dd82432bf6379c11abef1d2890b689991772
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-08-02 07:58:32 -0300

    Initial Commit

---
+++ Thinking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thinking
+//
+//  Created by kenjimaeda on 02/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
